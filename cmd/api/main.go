ab3e37e3cc33f213a2ef8725ce353a61e1363afa
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2025-07-11 00:24:29 -0500

    feat: implement graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"log/slog"
-	"net/http"
 	"os"
 	"strconv"
 	"time"
@@ -88,20 +87,12 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	err = app.serve()
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
 	}
-
-	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-
-	err = srv.ListenAndServe()
-	logger.Error(err.Error())
-	os.Exit(1)
 }
 
 func openDB(cfg config) (*pgxpool.Pool, error) {
---
+++ cmd/api/server.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
+	}
+
+	// Create a shutdownError channel. We will use this to receive any errors returned
+	// by the graceful Shutdown() function
+	shutdownError := make(chan error)
+
+	// start a background goroutine
+	go func() {
+		// create a quit channel which carries os.Signal values.
+		quit := make(chan os.Signal, 1)
+
+		// Use signal.Notify() to listen for incoming SIGINT and SIGTERM signals and
+		// relay them to the quit channel. Any other signals will not be caught by
+		// signal.Notify() and will retain their default behavior
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// Read the signal from the quit channel. This code will block until a signal is received
+		s := <-quit
+
+		// Log a message to say that t he signal has been caught. Notice that we also
+		// call the String() method on the signal to get the signal name and include it
+		// in the log entry attributes
+		app.logger.Info("shutting down server", "signal", s.String())
+
+		// Create a context with 30-second timeout
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		// Call Shutdown() on our server, passing in the context we just made.
+		// Shutdown() will return nil if the graceful shutdown was successfully or an
+		// error (which may happen because of a problem closing the listeners, or because the shutdown didn't complete
+		// before the 30-second context deadline is hit). We relay this return value to the shutdownError channel
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
+	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
+
+	// Calling Shutdown() on our server will cause ListenAndServe() to immediately
+	// return a http.ErrorServerClosed error. So if we see this error, it is actually a
+	// god thing and an indication that the graceful shutdown has started. So we check
+	// specifically for this, only returning the error if its is NOT http.ErrServerClosed.
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	// Otherwise, we wait to receive the return value from Shutdown() on the
+	// shutdownError channel. If the return value is an error, we know that there was a
+	// problem with the graceful shutdown and we return the error
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	// At this point we know that the graceful shutdown completed successfully and
+	// we log a "stopped server message"
+	app.logger.Info("stopped server", "addr", srv.Addr)
+
+	return nil
+}
